abb6d11e078ce0c33d584b2b2ea824072be780dc
swetha <gunaram194@gmail.com>
2026-01-08 11:45:31 +0530

    added:sweet_shop

--- myproject/settings.py
+++ myproject/settings.py
@@ -61,4 +61,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
