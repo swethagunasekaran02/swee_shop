87126883aa9f5038dc22c775270858e6df285576
swetha <gunaram194@gmail.com>
2026-01-08 11:59:23 +0530

    added

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,12 +21,7 @@ INSTALLED_APPS = [
     'home',
     
 ]
-INSTALLED_APPS += [
-    'cloudinary_storage',
-    'cloudinary',
-]
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
